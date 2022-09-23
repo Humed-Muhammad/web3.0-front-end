48ad74bffebd06755220d1f1aa6b9f603607c913
Humed-Muhammad <abc45171@gmail.com>
2022-09-23 10:43:29 +0300

    Initial

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Avatar } from "@chakra-ui/react";
+import reactLogo from "../assets/react.svg";
+export const Navbar = () => {
+  return (
+    <nav>
+      <Avatar src={reactLogo} />
+    </nav>
+  );
+};
